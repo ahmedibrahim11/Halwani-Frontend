5d96b1c77e0ec6830df10e8afa28653710976665
Shehab Harhash <shehab.harhash@vodafone.com>
2021-03-14 22:42:53 +0200

    adding submit to add ticket

--- src/app/create-ticket-popup/create-ticket-popup.component.ts
+++ src/app/create-ticket-popup/create-ticket-popup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder ,FormGroup, Validators} from '@angular/forms';
+import { createTicketDTO } from "../DTOs/createTicketDTO";
 @Component({
   selector: 'app-create-ticket-popup',
   templateUrl: './create-ticket-popup.component.html',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateTicketPopupComponent implements OnInit {
 
-  constructor() { }
+  createTicketDTO: createTicketDTO = new createTicketDTO();
+  createTicketDTOFormGroup: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+     this.createTicketDTOFormGroup = this.formBuilder.group({
+
+      
+      ticketType: ['', [Validators.required]],
+      summary: ['', [Validators.required]],
+      attachments:[''],
+      description:[''],
+      reporter:['', [Validators.required]],
+      source:['', [Validators.required]],
+      sevirity:['',[Validators.required]],
+      priority:['',[Validators.required]],
+      productCategoryName1:[''],
+      productCategoryName2:[''],
+    });
+  }
+  submiCreate(){
+console.log(this.createTicketDTOFormGroup.value );
+  }
+
+  uploadAll(){
+    console.log("Filesssss")
   }
 
 }
