e7ed604446151ba67afdf38591f51cfb16b7ed45
Shehab Harhash <shehab.harhash@vodafone.com>
2021-03-15 01:27:41 +0200

    fix file upload in create ticket

--- src/app/create-ticket-popup/create-ticket-popup.component.ts
+++ src/app/create-ticket-popup/create-ticket-popup.component.ts
@@ -1,24 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder ,FormGroup, Validators} from '@angular/forms';
 import { createTicketDTO } from "../DTOs/createTicketDTO";
+import { HTTPMainServiceService } from '../services/httpmain-service.service';
+import { NgxFileDropEntry, FileSystemFileEntry, FileSystemDirectoryEntry } from 'ngx-file-drop';
 @Component({
   selector: 'app-create-ticket-popup',
   templateUrl: './create-ticket-popup.component.html',
   styleUrls: ['./create-ticket-popup.component.css']
 })
 export class CreateTicketPopupComponent implements OnInit {
-
   createTicketDTO: createTicketDTO = new createTicketDTO();
   createTicketDTOFormGroup: FormGroup;
-  constructor(private formBuilder: FormBuilder) { }
-
+  constructor(private formBuilder: FormBuilder,private http: HTTPMainServiceService) { }
+private FileLinks;
   ngOnInit(): void {
      this.createTicketDTOFormGroup = this.formBuilder.group({
-
-      
       ticketType: ['', [Validators.required]],
       summary: ['', [Validators.required]],
-      attachments:[''],
       description:[''],
       reporter:['', [Validators.required]],
       source:['', [Validators.required]],
@@ -32,8 +30,54 @@ export class CreateTicketPopupComponent implements OnInit {
 console.log(this.createTicketDTOFormGroup.value );
   }
 
-  uploadAll(){
-    console.log("Filesssss")
+   public files: NgxFileDropEntry[] = [];
+
+  public dropped(files: NgxFileDropEntry[]) {
+    this.files = files;
+    for (const droppedFile of files) {
+
+      // Is it a file?
+      if (droppedFile.fileEntry.isFile) {
+        const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
+        fileEntry.file((file: File) => {
+
+          // Here you can access the real file
+          console.log(droppedFile.relativePath, file);
+
+         
+          const formData = new FormData()
+          formData.append(file.name, file)
+
+          
+
+         this.http.POST("Ticket/PostFile", formData)
+          .subscribe(data => {
+            this.FileLinks=data;
+            if(this.FileLinks.length!==0)
+            {
+              let newLink= data[0];
+              this.FileLinks.push(newLink);
+            }
+           console.log(this.FileLinks);
+          })
+         
+
+        });
+      } else {
+        // It was a directory (empty directories are added, otherwise only files)
+        const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
+        console.log(droppedFile.relativePath, fileEntry);
+      }
+    }
   }
+
+  public fileOver(event){
+    console.log(event);
+  }
+
+  public fileLeave(event){
+    console.log(event);
+  }
+
 
 }
