5d4b264386e7fafe7ccf190db157e94aaf463c71
Mostafa <mostafa.ibrahim2@vodafone.com>
2021-03-25 16:29:04 +0200

    fix-DTO

--- src/app/core/DTOs/createTicketDTO.ts
+++ src/app/core/DTOs/createTicketDTO.ts
@@ -1,6 +1,6 @@
-export class createTicketDTO{
+export class createTicketDTO {
   summary: string;
-  submitterTeam:string;
+  submitterTeam: string;
   submitterEmail: string;
   submitterName: string;
   team: string;
@@ -10,10 +10,11 @@ export class createTicketDTO{
   ticketStatus: number;
   description: string;
   submitDate: Date;
+  type: string;
   productCategoryName1: string;
   productCategoryName2: string;
   attachement: string;
-  priority:number;
-  source:number;
-  ServiceName:string
+  priority: number;
+  source: number;
+  ServiceName: string;
 }
