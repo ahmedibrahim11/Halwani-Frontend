b5e386869fe892082fdd231bea3979d40e89fa10
Mostafa <mostafa.ibrahim2@vodafone.com>
2021-03-25 18:08:06 +0200

    update-filter-popup

--- src/app/ITManager/filtermodal/filtermodal.component.ts
+++ src/app/ITManager/filtermodal/filtermodal.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-filtermodal',
   templateUrl: './filtermodal.component.html',
-  styleUrls: ['./filtermodal.component.css']
+  styleUrls: ['./filtermodal.component.css'],
 })
 export class FiltermodalComponent implements OnInit {
+  toppings = new FormControl();
 
-  constructor() { }
+  toppingList: string[] = [
+    'Extra cheese',
+    'Mushroom',
+    'Onion',
+    'Pepperoni',
+    'Sausage',
+    'Tomato',
+  ];
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
