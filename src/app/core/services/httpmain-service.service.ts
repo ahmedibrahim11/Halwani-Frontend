2445a155634d4560aee91bd3f36cfcb37e23db59
Mostafa <mostafa.ibrahim2@vodafone.com>
2021-03-29 20:46:45 +0200

    fix-login-issue

--- src/app/core/services/httpmain-service.service.ts
+++ src/app/core/services/httpmain-service.service.ts
@@ -2,15 +2,31 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HTTPMainServiceService {
-
-  constructor(private httpClient: HttpClient) { }
-   GET(route) {
-    return this.httpClient.get<any>(environment.serverURL+route,{headers:{Authorization:`Bearer ${JSON.parse(localStorage.getItem("userData")).token}`}});
+  constructor(private httpClient: HttpClient) {}
+  GET(route) {
+    return this.httpClient.get<any>(environment.serverURL + route, {
+      headers: {
+        Authorization: `Bearer ${
+          JSON.parse(localStorage.getItem('userData')).token
+        }`,
+      },
+    });
   }
-   POST(route,body) {
-    return this.httpClient.post<any>(environment.serverURL+route,body,{headers:{Authorization:`Bearer ${JSON.parse(localStorage.getItem("userData")).token}`}});
+  POST(route, body) {
+    console.log(localStorage.getItem('userData'));
+    if (localStorage.getItem('userData') !== null) {
+      return this.httpClient.post<any>(environment.serverURL + route, body, {
+        headers: {
+          Authorization: `Bearer ${
+            JSON.parse(localStorage.getItem('userData')).token
+          }`,
+        },
+      });
+    } else {
+      return this.httpClient.post<any>(environment.serverURL + route, body);
+    }
   }
 }
