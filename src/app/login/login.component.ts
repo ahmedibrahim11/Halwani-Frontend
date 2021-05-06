586c24b2556544f7951e0189c9ff262d21642f1e
Ahmed <ahmed.abdelkhalek1@vodafone.com>
2021-05-06 15:39:23 +0200

    fix changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {MatIconModule  } from "@angular/material/icon";
-import {MatFormFieldModule  } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from '@angular/material/input';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserLoginDTO } from "../core/DTOs/userLoginDTO";
 import { HTTPMainServiceService } from '../core/services/httpmain-service.service';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -14,55 +14,56 @@ import {MatSnackBar} from '@angular/material/snack-bar';
 })
 export class LoginComponent implements OnInit {
   durationInSeconds = 5;
-
-  
-hide = true;
-UserViewInfoObject: UserLoginDTO = new UserLoginDTO();
+  showSpinner: Boolean = false;
+  hide = true;
+  UserViewInfoObject: UserLoginDTO = new UserLoginDTO();
   UserViewInfoFormGroup: FormGroup;
 
 
-  constructor( private formBuilder: FormBuilder,
-     private http:HTTPMainServiceService,private _snackBar: MatSnackBar,
-     private router:Router) {
-    
-   }
+  constructor(private formBuilder: FormBuilder,
+    private http: HTTPMainServiceService, private _snackBar: MatSnackBar,
+    private router: Router) {
 
-  
+  }
+
+
   ngOnInit(): void {
-     this.UserViewInfoFormGroup = this.formBuilder.group({
+    this.UserViewInfoFormGroup = this.formBuilder.group({
 
-      
+
       email: ['', [Validators.required]],
       password: ['', [Validators.required]],
     });
   }
- 
-submitLogin()
-{
-  console.log(this.UserViewInfoFormGroup.value );
-  if(this.UserViewInfoFormGroup.valid)
-  {
-    this.http.POST(`Authentication/Login`,{
-      userName:this.UserViewInfoFormGroup.value.email,
-      password:this.UserViewInfoFormGroup.value.password
-    }).subscribe(data=>
-    {  console.log("data",data)
-      localStorage.setItem("userData",JSON.stringify(data))
-      switch (data.userProfile.roleEnum) {
-        case 0:
-          this.router.navigate(["/itmanager"])
-          break;
+
+  submitLogin() {
+    console.log(this.UserViewInfoFormGroup.value);
+    this.showSpinner = true;
+    if (this.UserViewInfoFormGroup.valid) {
+      this.http.POST(`Authentication/Login`, {
+        userName: this.UserViewInfoFormGroup.value.email,
+        password: this.UserViewInfoFormGroup.value.password
+      }).subscribe((data) => {
+        this.showSpinner = false;
+        console.log("data", data);
+        localStorage.setItem("userData", JSON.stringify(data))
+        switch (data.userProfile.roleEnum) {
+          case 0:
+            this.router.navigate(["/itmanager"])
+            break;
           case 1:
-          this.router.navigate(["/itpersonal"])
-          break;
+            this.router.navigate(["/itpersonal"])
+            break;
           case 2:
-          this.router.navigate(["/user"])
-          break;
-          
-      
-      }
-    },err => {this._snackBar.open("Username or password is incorrct","OK",{duration:this.durationInSeconds*1000});})
-    ///navigate depending on credentials
+            this.router.navigate(["/user"])
+            break;
+
+
+        }
+      }, err => { 
+        this.showSpinner=false;
+        this._snackBar.open("Username or password is incorrct", "OK", { duration: this.durationInSeconds * 1000 }); })
+      ///navigate depending on credentials
+    }
   }
-}
 }
