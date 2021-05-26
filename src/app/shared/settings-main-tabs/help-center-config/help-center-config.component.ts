08e8af5aad05a0087d6b9c42edbb3b3f33b9641a
Shehab Harhash <shehab.harhash@vodafone.com>
2021-05-26 16:30:25 +0200

    create add settings component first page "working frontend"

--- src/app/shared/settings-main-tabs/help-center-config/help-center-config.component.ts
+++ src/app/shared/settings-main-tabs/help-center-config/help-center-config.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatDialog } from '@angular/material/dialog';
+import { AddSettingsComponent } from './add-settings/add-settings.component';
 @Component({
   selector: 'app-help-center-config',
   templateUrl: './help-center-config.component.html',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelpCenterConfigComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog,) { }
 
   ngOnInit(): void {
   }
+openAddSettings() {
+    const dialogRef = this.dialog.open(AddSettingsComponent, {
+     
+      width:"35%",
+      height:"90%"
+    });
 
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
