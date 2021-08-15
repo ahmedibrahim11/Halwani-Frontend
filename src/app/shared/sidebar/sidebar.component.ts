f33ed3c02aa53de006be42dd731dabe1a8bdc008
Ahmed <ahmed.abdelkhalek1@vodafone.com>
2021-08-15 15:39:18 +0200

    fix logout

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -56,12 +56,12 @@ export class SidebarComponent implements OnInit {
     localStorage.clear();
     if (this.msalGuardConfig.interactionType === InteractionType.Popup) {
       this.authService.logoutPopup({
-        postLogoutRedirectUri: "http://localhost:4200/login",
-        mainWindowRedirectUri: "http://localhost:4200/"
+        postLogoutRedirectUri: "https://halwani-frontend.azurewebsites.net/login",
+        mainWindowRedirectUri: "https://halwani-frontend.azurewebsites.net/"
       });
     } else {
       this.authService.logoutRedirect({
-        postLogoutRedirectUri: "http://localhost:4200/",
+        postLogoutRedirectUri: "https://halwani-frontend.azurewebsites.net/login",
       });
     }
   }
